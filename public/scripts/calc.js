0f8f6ecf04c96b1e554548f8b00d7fb2fae3bc64
Brent Reno <brent.reno@gmail.com>
2016-09-10 15:53:53 -0500

    added bootstrap and css, started to style calculator

--- public/scripts/calc.js
+++ public/scripts/calc.js
@@ -1,8 +1,7 @@
 console.log(' Calc.js is sourced');
 var buttonData=[{name: 'zero', value:0}, {name: 'one', value:1},{name: 'two', value:2}, {name:'three', value:3},
-{name:'four', value:4}, {name:'five', value:5}, {name:'six', value:6}, {name:'seven', value:7}, {name:'eight', value:8},
-{name:'nine', value:9}, {name: 'add', value: '+'}, {name:'subtract', value:'-'},{name: "divide", value: '/'}, {name:'multiply', value: '*'},
-{name:'decimal', value:'.'}, {name: 'equals', value:'='} ];
+{name:'four', value:4}, {name:'five', value:5},  {name:'six', value:6}, {name:'seven', value:7},{name:'eight', value:8},
+{name:'nine', value:9}, {name:'decimal', value:'.'}, {name: 'add', value: '+'},{name:'subtract', value:'-'},{name: "divide", value: '/'}, {name:'multiply', value: '*'},{name: 'equals', value:'='}, { name:"clear", value:'C'} ];
 
 $(document).ready(function(){
   var numberDisplayText='';
@@ -28,6 +27,17 @@ $(document).ready(function(){
       console.log("data to be sent", dataObject);
       console.log('display',numberDisplayText);
     });//end function button on click
+
+    //clear button on click
+    $(".clearButton").click(function(){
+      console.log('In clear button');
+      dataObject.xValue ="";
+      dataObject.yValue="";
+      dataObject.Type="";
+      $('#calcDisplay').empty();
+      console.log("data objec:", dataObject);
+      numberDisplayText="";
+    });// end clear on click
 
     //equals button on click
     $('.equalsButton').on('click',function(){
@@ -93,22 +103,38 @@ $(document).ready(function(){
 
 var displayButtons = function(){
   var calcDivText = "";
+  //create buttons
   for (var i = 0; i < buttonData.length; i++) {
-    if(i%5 === 0 && i<16){
+
+    //line breaks if
+    if(i%3 === 0 && i<16){
      calcDivText += '<br>';
    } //end if
+   //equals button
     if(buttonData[ i ].value == '=' ){
-     calcDivText+='<button class="equalsButton" id="'+ buttonData[ i ].name +'" data-id='+ i +'>'+buttonData[ i ].value+ "</button>";
+     calcDivText+='<button class="equalsButton btn btn-default" id="'+ buttonData[ i ].name +'" data-id='+ i +'>'+buttonData[ i ].value+ "</button>";
      $('#calcDiv').html(calcDivText);
     }//end equals if
+    // operators buttons
     else if(buttonData[ i ].value == '+' ||buttonData[ i ].value == '-' ||buttonData[ i ].value == '/' ||buttonData[ i ].value == '*' ){
-      calcDivText +='<button class="functionButton" id="'+ buttonData[ i ].name +'" data-id='+ i +'>'+buttonData[ i ].value+ "</button>";
+      calcDivText +='<button class="functionButton btn btn-default" id="'+ buttonData[ i ].name +'" data-id='+ i +'>'+buttonData[ i ].value+ "</button>";
       $('#calcDiv').html(calcDivText);
-    }
+    }//end operators
+
+    // decimal button
       else if(buttonData[ i ].value =='.'){
-        calcDivText +='<button class="numberButton" id="'+ buttonData[ i ].name +'" data-id=".">'+buttonData[ i ].value+ "</button>";
-    } else {
-      calcDivText +='<button class="numberButton" id="'+ buttonData[ i ].name +'" data-id='+ i +'>'+buttonData[ i ].value+ "</button>";
+        calcDivText +='<button class=" btn btn-default numberButton " id="'+ buttonData[ i ].name +'" data-id=".">'+buttonData[ i ].value+ "</button>";
+    }//end decimal button
+
+    //clear button
+    else if(buttonData[ i ].value =="C") {
+      calcDivText +='<button  class="clearButton btn btn-default" id="'+ buttonData[ i ].name +'" data-id='+ i +'>'+buttonData[ i ].value+ "</button>";
+      $('#calcDiv').html(calcDivText);
+    }// end clear button
+
+    //number buttons
+    else {
+      calcDivText +='<button  class="numberButton btn btn-default" id="'+ buttonData[ i ].name +'" data-id='+ i +'>'+buttonData[ i ].value+ "</button>";
       $('#calcDiv').html(calcDivText);
     } //end if else
 
